29ed99604c64ac8bf89494c687fa5224661d5c2d
Xie Xianbin <xianbinxie@163.com>
2014-09-04 23:43:36 +0800

    add QRcode logo entity

---
+++ zxing/src/main/java/com/qikemi/packages/zxing/QRcodeLogoEntity.java
@@ -0,0 +1,11 @@
+package com.qikemi.packages.zxing;
+
+/**
+ * 
+ * Create Date: 2014年8月30日 下午11:38:22
+ * Author XieXianbin<a.b@hotmail.com>
+ * Source Repositories Address: <https://github.com/qikemi/open-wechat-sdk>
+ */
+public class QRcodeLogoEntity {
+	
+}
