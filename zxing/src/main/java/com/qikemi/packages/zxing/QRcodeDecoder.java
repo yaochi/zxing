1dc0973c8e453ad5991aa9fbe296dfc0186e4a0b
Xie Xianbin <xianbinxie@163.com>
2014-09-04 23:42:56 +0800

    init QRcodeDecoder

---
+++ zxing/src/main/java/com/qikemi/packages/zxing/QRcodeDecoder.java
@@ -0,0 +1,11 @@
+package com.qikemi.packages.zxing;
+
+/**
+ * 创建二维码 
+ * Create Date: 2014年8月30日 下午11:59:34
+ * Author XieXianbin<a.b@hotmail.com>
+ * Source Repositories Address: <https://github.com/qikemi/zxing>
+ */
+public class QRcodeDecoder {
+
+}
