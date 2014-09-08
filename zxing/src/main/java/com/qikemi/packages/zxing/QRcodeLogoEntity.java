97762ec5fc7d03d9bc8738be3aa7e0acfebc735b
Xie Xianbin <xianbinxie@163.com>
2014-09-08 22:49:30 +0800

    add logo configure

--- zxing/src/main/java/com/qikemi/packages/zxing/QRcodeLogoEntity.java
+++ zxing/src/main/java/com/qikemi/packages/zxing/QRcodeLogoEntity.java
@@ -1,11 +1,62 @@
 package com.qikemi.packages.zxing;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Stroke;
+
 /**
  * 
  * Create Date: 2014年8月30日 下午11:38:22
  * Author XieXianbin<a.b@hotmail.com>
- * Source Repositories Address: <https://github.com/qikemi/open-wechat-sdk>
+ * Source Repositories Address: <https://github.com/qikemi/zxing>
  */
 public class QRcodeLogoEntity {
+	
+	private String logoPath;
+	private Stroke stroke = new BasicStroke(2);
+	private Color color = Color.RED;
+	
+	// default 
+	public QRcodeLogoEntity() {
+		super();
+	}
+	
+	// sample 
+	public QRcodeLogoEntity(String logoPath) {
+		super();
+		this.logoPath = logoPath;
+	}
+	
+	// full 
+	public QRcodeLogoEntity(String logoPath, Stroke stroke, Color color) {
+		super();
+		this.logoPath = logoPath;
+		this.stroke = stroke;
+		this.color = color;
+	}
+
+	public String getLogoPath() {
+		return logoPath;
+	}
+
+	public void setLogoPath(String logoPath) {
+		this.logoPath = logoPath;
+	}
+
+	public Stroke getStroke() {
+		return stroke;
+	}
+
+	public void setStroke(Stroke stroke) {
+		this.stroke = stroke;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
 	
 }
